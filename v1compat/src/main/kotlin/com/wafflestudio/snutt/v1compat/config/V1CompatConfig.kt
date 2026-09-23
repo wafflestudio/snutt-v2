4d33630b3e65633122bca9a037e312627a92d946
Chanyeong Lim <asp345@posteo.net>
2026-09-23 20:32:59 +0900

    chore: CurrentClient 리졸버 중복 등록과 배포 job 이름 정리
    v1compat은 api 애플리케이션에서만 실행되므로 WebConfig의 등록 하나로 충분하다. 배포 job 이름의 matrix 식은 job이 건너뛰어질 때 치환되지 않아 식 그대로 표시되므로 제거한다.

--- v1compat/src/main/kotlin/com/wafflestudio/snutt/v1compat/config/V1CompatConfig.kt
+++ v1compat/src/main/kotlin/com/wafflestudio/snutt/v1compat/config/V1CompatConfig.kt
@@ -1,6 +1,5 @@
 package com.wafflestudio.snutt.v1compat.config
 
-import com.wafflestudio.snutt.core.common.client.CurrentClientArgumentResolver
 import com.wafflestudio.snutt.core.domain.user.model.User
 import com.wafflestudio.snutt.v1compat.auth.V1ApiKeyInterceptor
 import com.wafflestudio.snutt.v1compat.auth.V1CurrentUser
@@ -30,7 +29,6 @@ class V1CompatConfig(
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(currentUserArgumentResolver)
-        resolvers.add(CurrentClientArgumentResolver)
     }
 
     companion object {
